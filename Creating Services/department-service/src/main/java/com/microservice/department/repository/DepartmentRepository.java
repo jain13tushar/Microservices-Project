603a494302e4f306188b012339cb5bd9bb52ad31
Jain <TJ069482@cerner.net>
2021-10-03 16:37:17 +0530

    Microservice self understanding project

--- Creating Services/department-service/src/main/java/com/microservice/department/repository/DepartmentRepository.java
+++ Creating Services/department-service/src/main/java/com/microservice/department/repository/DepartmentRepository.java
@@ -4,6 +4,9 @@ import com.microservice.department.entity.Department;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+// @Repository is a Spring annotation that indicates that the decorated class is a repository.
+// A repository is a mechanism for encapsulating storage, retrieval, and search behavior
+// which emulates a collection of objects.
 @Repository
 public interface DepartmentRepository extends JpaRepository<Department,Long> {
 
--- Creating Services/department-service/src/main/java/com/microservice/department/service/DepartmentService.java
+++ Creating Services/department-service/src/main/java/com/microservice/department/service/DepartmentService.java
@@ -6,6 +6,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+// @Service annotation is used with classes that provide some business functionalities.
+// Spring context will autodetect these classes when annotation-based configuration
+// and classpath scanning is used.
 @Service
 @Slf4j
 public class DepartmentService {
